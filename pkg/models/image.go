70085a8fa3ede71d16bffd1ed5019b64ba285c8e
David Pello <dvpg@tuta.io>
2025-04-29 16:07:06 +0200

    Image model to interact with the database

--- pkg/models/image.go
+++ pkg/models/image.go
@@ -1,7 +1,10 @@
 package models
 
+import "time"
+
 type Image struct {
 	ID       uint   `gorm:"primaryKey"`
 	FileName string `json:"filename" gorm:"unique"`
+	DateTime time.Time
 	Mission  string
 }
