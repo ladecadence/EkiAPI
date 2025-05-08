d6e67a935c48a549c150c51ae523339686b266b8
David Pello <dvpg@tuta.io>
2025-05-08 12:01:58 +0200

    Use a explicit flusher and check if it is available

--- pkg/controllers/events.go
+++ pkg/controllers/events.go
@@ -12,19 +12,25 @@ func ApiEvents(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Cache-Control", "no-cache")
 	writer.Header().Set("Connection", "keep-alive")
 
-	// subscribe to msgbus
-	updateCh := updateBus.Subscribe()
-
 	// Create a context for handling client disconnection
 	_, cancel := context.WithCancel(request.Context())
 	defer cancel()
+
+	flusher, ok := writer.(http.Flusher)
+	if !ok {
+		http.Error(writer, "Streaming unsupported", http.StatusInternalServerError)
+		return
+	}
+
+	// subscribe to msgbus
+	updateCh := updateBus.Subscribe()
 
 	// Send data to the client when we receive an event
 	for {
 		select {
 		case data := <-updateCh:
 			fmt.Fprintf(writer, "data: %s\n\n", data)
-			writer.(http.Flusher).Flush()
+			flusher.Flush()
 		}
 	}
 
